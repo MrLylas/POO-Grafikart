609fcea7a7a02fcf0bc60ff7f337c76daaf17505
MrLylas <delmas.denilson.pro@gmail.com>
2024-11-13 21:55:49 +0100

    Mise en place d'un formulaire php

--- Formulaire/Form.php
+++ Formulaire/Form.php
@@ -1,19 +1,40 @@
 <h1>Formulaire</h1>
 
 <?php
+/**
+ * permet de générer un formulaire simplement
+ */
+
 
 class Form{
-
+    /**
+     * @var array Données utilisées par le formlaire
+     */
     private $data;
+    /**
+     * @var string Tag utilisé pour entourer les champs
+     */
     public $surround = 'p';
+
+    /**
+     * @param array $data Données utilisées par le formlaire
+     */
+     */
 
     public function __construct($data = array()){
         $this->data = $data;
     }
-
+    /**
+     * @param $html string Code HTML à entourer
+     * @return string
+     */
     private function surround($html){
         return "<{$this->surround}>{$html}</{$this->surround}>";
     }
+        /**
+     * @param $index string
+     * @return string
+     */
     private function getValue($index){
         return isset($this->data[$index]) ? $this->data[$index] : null;
     }
